deffeb7e121af63c115fceb5cbdd5097e6717c86
Josh Chang <joshchang04@gmail.com>
2022-12-03 14:16:53 -0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,183 @@
+<template>
+  <Html class="bg-slate-100">
+
+  </Html>
+  <div class="absolute inset-14 grid items-center w-full max-w-5xl mx-auto">
+    <div class="grid items-center justify-between font-serif grid-flow-col gap-2 self-end mb-8">
+      <div class="grid gap-2">
+        <div class="text-xl uppercase tracking-widest text-slate-600">
+          Central Asia
+        </div>
+        <div class="text-4xl font-bold text-slate-900">
+          Is Geography Destiny?
+        </div>
+      </div>
+      <NuxtLink
+        to="/about"
+        class="text-xl text-slate-500 hover:text-slate-600 bg-slate-200 hover:bg-slate-300 transition-colors hover:cursor-pointer pt-3 pb-2 px-8 rounded-full">
+        About this project
+      </NuxtLink>
+    </div>
+    <div class="relative border-2 border-slate-500 overflow-hidden rounded-2xl self-start h-[70vh] max-h-full">
+      <l-map style="max-height: 100% !important;" :zoom="5" :minZoom="5" :maxZoom="10" :zoomAnimation="true" :center="[45.6, 68]" :markerZoomAnimation="true"
+        :maxBounds="[
+          [35.21639549928713, 90.44351810932858],
+          [55.64710121676032, 45.45447164119719],
+        ]">
+        <l-image-overlay
+          :url="image"
+          :bounds="bounds"
+          :options="imageOptions"></l-image-overlay>
+        <l-geo-json
+          :geojson="geojson"
+          :options="geojsonOptions"></l-geo-json>
+      </l-map>
+    </div>
+    <NuxtPage />
+  </div>
+</template>
+
+<script>
+  import { LMap, LGeoJson, LImageOverlay } from "@vue-leaflet/vue-leaflet";
+
+  export default {
+    components: {
+      LMap,
+      LGeoJson,
+      LImageOverlay,
+    },
+    data() {
+      return {
+        geojson: {
+          type: "FeatureCollection",
+          features: [
+            {
+              "type": "Feature",
+              "properties": {
+                "name": "yurt",
+                "image": "artifacts/yurt.jpg"
+              },
+              "geometry": {
+                "type": "Point",
+                "coordinates": [61, 50]
+              }
+            },
+            {
+              "type": "Feature",
+              "properties": {
+                "name": "bazaar",
+                "image": "artifacts/bazaar.png"
+              },
+              "geometry": {
+                "type": "Point",
+                "coordinates": [73, 40.5]
+              }
+            },
+            {
+              "type": "Feature",
+              "properties": {
+                "name": "mosque",
+                "image": "artifacts/mosque.png"
+              },
+              "geometry": {
+                "type": "Point",
+                "coordinates": [71.5, 51.16]
+              }
+            },
+            {
+              "type": "Feature",
+              "properties": {
+                "name": "plov",
+                "image": "artifacts/plov.jpg"
+              },
+              "geometry": {
+                "type": "Point",
+                "coordinates": [64, 42]
+              }
+            },
+            {
+              "type": "Feature",
+              "properties": {
+                "name": "tembur",
+                "image": "artifacts/tembur.jpg"
+              },
+              "geometry": {
+                "type": "Point",
+                "coordinates": [80, 42]
+              }
+            }
+          ],
+        },
+        geojsonOptions: {
+          // Options that don't rely on Leaflet methods.
+        },
+        image: "terrain3.png",
+        bounds: [
+          [35.21639549928713, 45.45447164119719],
+          [55.64710121676032, 90.44351810932858],
+        ],
+        imageOptions: {
+          // Options that don't rely on Leaflet methods.
+        },
+      };
+    },
+    async beforeMount() {
+      // HERE is where to load Leaflet components!
+      const { marker, icon } = await import("leaflet/dist/leaflet-src.esm");
+
+      // And now the Leaflet circleMarker function can be used by the options:
+      this.geojsonOptions.pointToLayer = (feature, latLng) => {
+        return marker(latLng, {
+          icon: icon({
+            iconUrl: feature.properties.image,
+            iconSize: [70, 70],
+            iconAnchor: [35, 35],
+            className: "click-" + feature.properties.name,
+          })
+        });
+      }
+      this.mapIsReady = true;
+    },
+    async mounted() {
+      for(let name of ["yurt", "bazaar", "mosque", "plov", "tembur"]) {
+      
+        const interval = setInterval(() => {
+          const artifact = document.querySelector(".click-" + name);
+          if (artifact) {
+            artifact.addEventListener("click", () => {
+              console.log(name)
+              this.$router.push("/" + name);
+            })
+            clearInterval(interval)
+          }
+        }, 100)
+      }
+    }
+  };
+</script>
+
+<style>
+  .leaflet-touch .leaflet-control-layers,
+  .leaflet-touch .leaflet-bar {
+    @apply border-none shadow-lg rounded-lg overflow-hidden;
+  }
+
+  .leaflet-touch .leaflet-bar a span {
+    @apply relative bottom-[2px];
+  }
+
+  .leaflet-control-attribution {
+    @apply hidden;
+  }
+
+  .leaflet-interactive {
+    @apply drop-shadow-lg;
+  }
+
+  .leaflet-marker-icon {
+    @apply border-white border-4 rounded-full;
+  }
+  .leaflet-top, .leaflet-bottom {
+    z-index: 1000;
+  }
+</style>
---
+++ pages/index/plov.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="modal modal-open font-serif z-[1001]">
+      <div class="modal-box w-11/12 max-w-4xl p-24">
+        <h3 class="font-bold text-5xl pb-6">Plov</h3>
+        <p class="py-4 text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+          in
+          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+          officia
+          deserunt mollit anim id est laborum.</p>
+        <p class="py-4 text-2xl">Enim nec dui nunc mattis enim ut tellus. Amet luctus venenatis lectus magna. Fusce id velit ut tortor
+          pretium viverra suspendisse. Tortor id
+          aliquet lectus proin. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Nibh sit amet commodo nulla facilisi.
+          Quam
+          vulputate dignissim suspendisse in est. Augue ut lectus arcu bibendum at varius vel. Erat pellentesque adipiscing commodo elit at
+          imperdiet
+          dui accumsan sit. Sit amet nisl purus in mollis nunc. Cursus turpis massa tincidunt dui ut. Purus ut faucibus pulvinar elementum
+          integer
+          enim neque volutpat ac. Pharetra massa massa ultricies mi quis hendrerit dolor magna eget.</p>
+        <p class="py-4 text-2xl">Consectetur a erat nam at lectus urna. Ultricies integer quis auctor elit sed. Sed felis eget velit aliquet
+          sagittis id consectetur purus
+          ut. Nec sagittis aliquam malesuada bibendum arcu vitae elementum. Massa vitae tortor condimentum lacinia quis vel. Suspendisse
+          ultrices
+          gravida dictum fusce ut. Vulputate enim nulla aliquet porttitor lacus luctus. Morbi blandit cursus risus at ultrices mi tempus.
+          Praesent
+          elementum facilisis leo vel fringilla est. Ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum.</p>
+        <div class="modal-action">
+          <NuxtLink to="/">
+            <label for="my-modal-5" class="btn px-7 pb-1 pt-2 text-lg tracking-wide">Back</label>
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
