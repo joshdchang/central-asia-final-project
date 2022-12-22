6be16c7345d8d786b4b8be329807fd946d0129d7
Josh Chang <joshchang04@gmail.com>
2022-12-22 00:29:27 -0500

    bib

--- pages/index/yurt.vue
+++ pages/index/yurt.vue
@@ -1,84 +1,109 @@
-
 <template>
   <div>
     <div class="modal modal-open font-serif z-[1001]">
       <div class="modal-box w-11/12 max-w-4xl p-24">
         <h3 class="font-bold text-5xl pb-6">The Yurt</h3>
-          <p class="py-4 text-2xl">
-            The yurt, a type of dwelling used by nomadic and semi-nomadic people, is one of the most recognizable symbols of nomadic life on
-            the Central Asia steppe. Yurts come in different varieties,
-            but most
-            have generally the same structure. In a paper titled “​​Architecture of the yurt and its impact on teaching young architects,”
-            Zhandarbek
-            Malibekuly writes, “Basically, the yurt is divided into three structural parts: the kerege, uyk and shanyrak.”
-          </p>
-          <p class="py-4 text-2xl">
-            According to Malibekuly, “The kerege is a bearing structural element,…. The uyk is a constructive element that rests on a kerege
-            base under
-            an incline, and is fixed at the end to support the shanyrak. The insulation of what is called uzіk felt, rests on the uyk and
-            provides heat
-            inside the yurt; at the same time as protecting it from precipitation, e.g. snow and rain. The shanyrak is the upper part of the
-            yurt.
-            Dome-shaped, the shanyrak protects the yurt from pressure in the form of snow and rain because of its shape. The round,
-            streamlined shape of
-            a truncated cone provides protection from strong winds.”
-          </p>
-          <p class="py-4 text-2xl">
-            From this description, it is clear that the yurt and its design are largely a function of the environment in which it exists.
-            The
-            overall
-            form of the yurt is to fight against “strong winds,” and other major aspects of the design combat the precipitation of the
-            steppe,
-            including
-            snow, rain.
-          </p>
-          <p class="py-4 text-2xl">
-            Perhaps the best evidence of the yurt as a function of its environment are the variations of yurts from place to place.
-            Malibekuly
-            writes,
-            “In the Mongolian steppe, climatic conditions vary with sultry, strong winds whereas mountainous areas with rocky slopes are
-            characteristic
-            of Kyrgyzstan. Residential settlements are located mainly between the mountain ranges. The characteristic shape of the
-            configuration of the
-            Kyrgyz yurt is elongated and pointed. The yurts of the mountainous Altai retained the type of a classical hut or polygonal log
-            ail. Forests
-            and mountains cover the area from Altai to the Caspian Sea between about 45⁰ and 50⁰ north latitude. In addition, in the vast
-            expanses of
-            the Kazakh steppe, the yurts differ, but the overall proportions of the Kazakh yurts differ from others in the neighboring
-            states
-            of Russia,
-            Kyrgyzstan, Mongolia and others.” (Malibekuly et al).
-          </p>
-          <p class="py-4 text-2xl">
-            From the steppe to the mountains to the forest, Malibekuly shows that the physical environment is perhaps the single most
-            important factor
-            guiding the differences between yurts across regions. Precipitation, wind, vegetation, topography, and latitude all have a
-            profound
-            influence on the yurt’s shape and structure.
-          </p>
-          <p class="py-4 text-2xl">
-            The yurt, however, is not simply a utilitarian object. The yurt is also a place of art and spiritual culture. According to
-            Yelena Yurievna
-            Lichman, “The Kazakh yurt is not only an element of material culture of the folk, but also contains symbolism, in which
-            different
-            information about religious and mythological image of the world is focused. In Kazakhs folk image of the world available to
-            usual conscious,
-            such external features of the surrounding world as the sky, the Sun, the stars and the steppe served as a model for the outer
-            and inner
-            space of yurt.” Even the spiritual aspects of the yurt are connected to the land and the environment. According to Lichman, the
-            “surrounding
-            world” is central to the image of the yurt in a religious and mythological sense.
-          </p>
-          <p class="py-4 text-2xl">
-            The yurt is defined and characterized, perhaps most clearly of all the artifacts in this project, by the physical landscape it
-            exists in and
-            the climatic conditions that it protects its inhabitants from. The yurt, an essential part of Central Asian nomadic and
-            semi-nomadic
-            culture, is a dwelling place, but more than that, it is a grounded symbol of identity amidst a constantly changing world.
-          </p>
-          <p class="py-6 text-lg">
-            Note: the bibliography will be here, but I'm going to wait until I after I finish editing the content in case things change.
-          </p>
+        <p class="py-4 text-2xl">
+          The yurt, a type of dwelling used by nomadic and semi-nomadic people, is one of the most recognizable symbols of nomadic life on
+          the Central
+          Asia steppe. Yurts come in different varieties, but most have generally the same structure.
+        </p>
+        <p class="py-4 text-2xl">
+          There are three main structural components of the yurt. First, the “kerege,” which is a “bearing structural element” that makes up
+          the
+          walls. Then, there are the “uyk,” which are poles that create dome structure for the roof. Finally, there is the “shanyrak,” which
+          is a
+          round rim that holds together the top of the dome [1].
+        </p>
+        <div class="py-4">
+          <img src="/yurt.jpg" class="w-full rounded-lg">
+          <figcaption class="mt-3">Source: Yurt Info [2]</figcaption>
+        </div>
+        <p class="py-4 text-2xl">
+          Despite its simplicity, the yurt is designed to be incredibly sturdy and resistant to the elements. Malibekuly writes, “...the
+          shanyrak
+          protects the yurt from pressure in the form of snow and rain because of its shape. The round, streamlined shape of a truncated
+          cone provides
+          protection from strong winds” [2]. From this description, it is clear that the yurt and its design are largely a result of the
+          environment
+          in which it exists. The overall dome-like form of the yurt is to fight against “strong winds,” and other major aspects of the
+          design combat
+          the precipitation of the steppe, including snow, rain.
+        </p>
+        <p class="py-4 text-2xl">
+          Perhaps the best evidence of the yurt as a function of its environment are the variations of yurts from place to place. On the
+          Mongolian
+          steppe, for instance, the “climatic conditions vary with sultry, strong winds,” the yurts are “the type of a classical hut or
+          polygonal log
+          ail” [3]. This differs from mountainous, “rocky slopes” of much of Kyrgyzstan, where the yurts are usually “elongated and pointed”
+          [4].
+        </p>
+        <p class="py-4 text-2xl">
+          From the steppe to the mountains to the forest, Malibekuly demonstrates that the physical environment is perhaps the single most
+          important
+          factor guiding the differences between yurts across regions. Precipitation, wind, vegetation, topography, and latitude all have a
+          profound
+          influence on the yurt’s shape and structure. This is, perhaps, unsurprising. Nomadic people live away from settled populations, so
+          they must
+          rely on the yurt as a safe dwelling no matter the conditions. That means adapting to the geographic environment and designing the
+          yurt to
+          match the specific natural forces it will face.
+        </p>
+        <p class="py-4 text-2xl">
+          The yurt, however, is not simply a utilitarian object. The yurt is also a place of art and spiritual culture. According to Yelena
+          Lichman,
+          “In Kazakhs folk image of the world available to usual conscious, such external features of the surrounding world as the sky, the
+          Sun, the
+          stars and the steppe served as a model for the outer and inner space of yurt” [5]. This demonstrates that even the spiritual
+          aspects of the
+          yurt are connected to the land and the environment. The “surrounding world” is central to the image of the yurt in a religious and
+          mythological sense, and the “steppe,” the physical biome of the yurt, is one of the most important pieces of folk spiritual
+          culture of the
+          nomadic people who live within it.
+        </p>
+        <p class="py-4 text-2xl">
+          The yurt is defined and characterized by the physical landscape it exists in and the climatic conditions that it protects its
+          inhabitants
+          from. An essential part of Central Asian nomadic and semi-nomadic culture, the yurt is a dwelling place, but beyond that, it is a
+          grounded
+          symbol of identity amidst a constantly changing world.
+        </p>
+        <p class="py-8">
+          1. Zhandarbek Malibekuly et al., “Architecture of the Yurt and Its Impact on Teaching Young Architects,” World Transactions on
+          Engineering and
+          Technology Education 18, no. 2 (2020): pp. 225-230, 227.
+          <br>
+          2. Becky Kemery, Yurt Info, 2006, https://www.yurtinfo.org/images/faq09.jpg.
+          <br>
+          3. Malibekuly, 227
+          <br>
+          4. Malibekuly, 227
+          <br>
+          5. Malibekuly, 227
+          <br>
+          6. Yelena Yurievna Lichman and Tamara Mikhailovna Doroshenko, “Traditions of Art and Craft Decoration and Architecture of Kazakh
+          Yurt as a
+          Source of Modern Art,” European Journal of Science and Theology 12, no. 3 (2106): pp. 221-232, 223.
+          <br>
+        </p>
+        <p class="py-4 text-2xl">
+          Bibliography
+        </p>
+        <p class="py-2 -indent-10 pl-10">
+          Kemery, Becky. Diagram from “YURTS: Living in the Round” Showing the Forces of Tension and Compression in the Yurt Roof Structure.
+          2006.
+          Yurt Info. https://www.yurtinfo.org/images/faq09.jpg.
+        </p>
+        <p class="py-2 -indent-10 pl-10">
+          Lichman, Yelena Yurievna, and Tamara Mikhailovna Doroshenko. “Traditions of Art and Craft Decoration and Architecture of Kazakh
+          Yurt as a
+          Source of Modern Art.” European Journal of Science and Theology 12, no. 3 (2106): 221–32.
+        </p>
+        <p class="py-2 -indent-10 pl-10">
+          Malibekuly, Zhandarbek, Olga Semenyuk, Ulkairbek Dyisebayev, and Sabina Kuc. “Architecture of the Yurt and Its Impact on Teaching
+          Young
+          Architects.” World Transactions on Engineering and Technology Education 18, no. 2 (2020): 225–30.
+        </p>
         <div class="modal-action">
           <NuxtLink to="/">
             <label for="my-modal-5" class="btn px-7 pb-1 pt-2 text-lg tracking-wide">Back</label>
