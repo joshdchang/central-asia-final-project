3dfc6025c3b8214b41c87ec66d4234df16f2d8f4
Josh Chang <joshchang04@gmail.com>
2022-12-22 02:01:43 -0500

    c

--- pages/index/sulaiman.vue
+++ pages/index/sulaiman.vue
@@ -23,18 +23,18 @@
           spots
           on the mountain that have different properties with regards to different ailments [4]. One specific hole in the rock is used for
           “sticking a
-          sore arm all the way in to remove pain,” a cave for “infertile women to enter in order to become able to conceive children,” and
-          “a slide
+          sore arm all the way in to remove pain,” there is a cave for “infertile women to enter in order to become able to conceive
+          children,” and there is “a slide
           of polished rock said to cure back aches” [5]. Women often walk around the mountain, utilizing the different topographic and
           geologic
-          features prescribed for their particular ailments [6], showing the power and significance people ascribe directly to the physical
+          features for their particular ailments [6], showing the power people ascribe directly to the physical
           geography
           of the mountain.
         </p>
         <p class="py-4 text-2xl">
           The mountain also has spiritual importance beyond its supposed healing abilities. Sulaiman-Too’s name comes from King Solomon, a
           prophet in
-          the Qur’an who is said to have various connections to the shrine [7]. However, Sulaiman-Too also had religious significance long
+          the Qur’an who is said to have various connections to the shrine [7]. However, Sulaiman-Too had significance long
           before
           Islam moved into the region. Claudius Ptolemy, an intellectual and traveler from Alexandria Egypt who lived from around 100 to 170
           AD, wrote
@@ -47,21 +47,20 @@
           in the
           context of how large of a role physical geography plays in cultures across Central Asia and elsewhere. When the land has such a
           large impact
-          on existence in other respects, it is not hard to understand how it might be perceived to have power in other domains, such as
-          health and
-          religion. There is perhaps nothing more spiritual and mystical than the grandeur of the mountains, which provide a constant
-          reminder of how
-          small we are and how little control we have over the land we inhabit.
+          on existence in other respects, it is not hard to understand how it might be perceived to have power in domains such as
+          health and religion.
         </p>
         <p class="py-8">
-          1. UNESCO World Heritage Centre, “Sacred Mountain in Kyrgyzstan Enters List along with Iran's Shushtar Water System and Royal Tombs in Republic
+          1. UNESCO World Heritage Centre, “Sacred Mountain in Kyrgyzstan Enters List along with Iran's Shushtar Water System and Royal
+          Tombs in Republic
           of Korea,” UNESCO World Heritage Centre (UNESCO World Heritage Centre, June 29, 2009), https://whc.unesco.org/en/news/529.
           <br>
           2. Gawron, Christian. View of the Mountain from the City of Osh. Wikipedia. Wikimedia Commons, September 21, 2005.
           https://en.wikipedia.org/wiki/Sulayman_Mountain#/media/File:Kyrgyzstan_Osh_with_Suleiman_Hill.jpg.
           <br>
           3. Morgan Y Liu, “A Central Asian Tale of Two Cities: Locating Lives and Aspirations in a Shifting
-          Post-Soviet Cityscape,” in Everyday Life in Central Asia: Past and Present (Bloomington: Indiana University Press, 2007), pp. 66-83, 73.
+          Post-Soviet Cityscape,” in Everyday Life in Central Asia: Past and Present (Bloomington: Indiana University Press, 2007), pp.
+          66-83, 73.
           <br>
           4. Bernbaum, Edwin. “Central Asia: The Distant Ranges.” Chapter. In Sacred Mountains of the World, 2nd ed., (Cambridge: Cambridge
           University Press, 2022), pp. 67–79, 68.
@@ -72,7 +71,8 @@
           <br>
           7. Bernbaum, 68.
           <br>
-          8. Riaz Dean, “The Location of Ptolemy’s Stone Tower: the Case for Sulaiman-Too in Osh,” The Silk Road, Vol 13., 2015, pp. 75-83, 75.
+          8. Riaz Dean, “The Location of Ptolemy’s Stone Tower: the Case for Sulaiman-Too in Osh,” The Silk Road, Vol 13., 2015, pp. 75-83,
+          75.
           <br>
           9. Bernbaum, 68.
         </p>
@@ -80,7 +80,8 @@
           Bibliography
         </p>
         <p class="py-2 -indent-10 pl-10">
-          UNESCO World Heritage Centre. “Sacred Mountain in Kyrgyzstan Enters List along with Iran's Shushtar Water System and Royal Tombs in
+          UNESCO World Heritage Centre. “Sacred Mountain in Kyrgyzstan Enters List along with Iran's Shushtar Water System and Royal Tombs
+          in
           Republic of Korea.” UNESCO World Heritage Centre. UNESCO World Heritage Centre, June 29, 2009. https://whc.unesco.org/en/news/529.
         </p>
         <p class="py-2 -indent-10 pl-10">
@@ -88,11 +89,14 @@
           https://en.wikipedia.org/wiki/Sulayman_Mountain#/media/File:Kyrgyzstan_Osh_with_Suleiman_Hill.jpg.
         </p>
         <p class="py-2 -indent-10 pl-10">
-          Sahadeo, Jeff, Russell G. Zanca, and Morgan Y Liu. “A Central Asian Tale of Two Cities: Locating Lives and Aspirations in a Shifting
-          Post-Soviet Cityscape.” Essay. In Everyday Life in Central Asia: Past and Present, 66-83. Bloomington: Indiana University Press, 2007.
+          Sahadeo, Jeff, Russell G. Zanca, and Morgan Y Liu. “A Central Asian Tale of Two Cities: Locating Lives and Aspirations in a
+          Shifting
+          Post-Soviet Cityscape.” Essay. In Everyday Life in Central Asia: Past and Present, 66-83. Bloomington: Indiana University Press,
+          2007.
         </p>
         <p class="py-2 -indent-10 pl-10">
-          Bernbaum, Edwin. “Central Asia: The Distant Ranges.” Chapter. In Sacred Mountains of the World, 2nd ed., 67–79. Cambridge: Cambridge
+          Bernbaum, Edwin. “Central Asia: The Distant Ranges.” Chapter. In Sacred Mountains of the World, 2nd ed., 67–79. Cambridge:
+          Cambridge
           University Press, 2022. doi:10.1017/9781108873307.005.
         </p>
         <p class="py-2 -indent-10 pl-10">
